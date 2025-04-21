1236057cd5786d50be30244e89b497f461d1a4e8
S-furi <stefano.furi@studio.unibo.it>
2025-04-21 16:35:51 +0200

    feat: try to locate local kotlin stdlib before downloading from maven

--- src/main/kotlin/io/github/sfuri/KSuggest.kt
+++ src/main/kotlin/io/github/sfuri/KSuggest.kt
@@ -12,7 +12,7 @@ import java.util.jar.JarFile
 class KSuggest {
     private val jarName = "kotlin-stdlib-2.1.10.jar"
     private val ktStdLibMvn = "https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/2.1.10/$jarName"
-    private val ktStdLibPath= listOf("src", "main", "resources", jarName).joinToString(File.separator)
+    private var ktStdLibPath: String? = Class.forName("kotlin.Unit").protectionDomain.codeSource.location?.let { it.toString().replace("^file:".toRegex(), "") }
 
     fun simpleLazyFinder(name: String): Sequence<String> = lazyStdLibDeclarations().filter { it.simpleName.startsWith(name) }.map { it.name }
 
@@ -27,20 +27,23 @@ class KSuggest {
     }
 
     private fun getKtStdLib() {
-        if (File(this.ktStdLibPath).exists()) {
+        this.ktStdLibPath = listOf("src", "main", "resources", jarName).joinToString(File.separator)
+
+        if (File(this.ktStdLibPath!!).exists()) {
             println("$jarName already exists")
             return
         }
+
         val connection = (URI(this.ktStdLibMvn).toURL().openConnection() as HttpURLConnection).also { it.requestMethod = "GET" }
         val jar = connection.inputStream.buffered().use { it.readAllBytes() }
-        File(this.ktStdLibPath).writeBytes(jar)
+        File(this.ktStdLibPath!!).writeBytes(jar)
         println("$jarName downloaded successfully at ${this.ktStdLibPath}")
     }
 
     private fun lazyStdLibDeclarations() = sequence<Class<*>> {
         ktStdLibIsPresent()
         val jarFile = try {
-            File(ktStdLibPath)
+            File(ktStdLibPath!!)
         } catch (e: NullPointerException) {
             throw IllegalStateException("Something went wrong")
         }
@@ -58,7 +61,7 @@ class KSuggest {
         }
     }
 
-    private fun ktStdLibIsPresent() = File(ktStdLibPath).exists().takeIf { it } ?: getKtStdLib()
+    private fun ktStdLibIsPresent() = (ktStdLibPath != null && File(ktStdLibPath!!).exists()).takeIf { it } ?: getKtStdLib()
 }
 
 
@@ -70,8 +73,6 @@ fun main(args: Array<String>) {
 
     val nameToFind = args[0]
     val kSuggest = KSuggest()
-    println("Simple Lazy Finder results: ")
-    kSuggest.simpleLazyFinder(nameToFind).forEach(::println)
-    println("Concurrent Lazy Finder results: ")
+    // kSuggest.simpleLazyFinder(nameToFind).forEach(::println)
     kSuggest.concurrentLazyFinder(nameToFind, bufSize = 100).forEach(::println)
 }
